a3695114aa8ad284eed361448fcbfe6bb42397b4
Jannis R <mail@jannisr.de>
2020-04-11 18:24:43 +0200

    implementation :clap:

--- api/latest.js
+++ api/latest.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const {promisify} = require('util')
+const createCors = require('cors')
+const fetchLatestUrl = require('../lib/fetch-latest-url')
+
+const cors = promisify(createCors())
+
 const latestDelfiGtfsUrl = async (req, res) => {
-	// todo
+	await cors(req, res)
+	if (res.headersSent) return;
+
+	res.setHeader('content-type', 'text-plain')
+
+	const url = await fetchLatestUrl()
+	if (url) {
+		res.status(302)
+		res.setHeader('location', url)
+		res.end(url)
+	} else {
+		res.status(404)
+		res.end('failed to determine the latest valid URL')
+	}
 }
 
 module.exports = latestDelfiGtfsUrl
